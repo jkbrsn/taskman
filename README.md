d7134cf84a40787efcfc80b8cd7256ce5a022227
Jakob Ersson <11717405+jakobilobi@users.noreply.github.com>
2024-11-29 08:17:14 +0100

    Add package code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # go-taskman
+
+A simple but efficient task manager for Go.
---
+++ task.go
@@ -0,0 +1,15 @@
+package taskman
+
+// Result represents the result of a task execution.
+// TODO: interfacify this too?
+type Result struct {
+	Data    map[string]interface{}
+	Error   error
+	Success bool
+}
+
+// Task is an interface for tasks that can be executed.
+// TODO: consider adding a context.Context parameter to Execute, to handle timeouts and cancellation (can also be forcefully added in the worker)
+type Task interface {
+	Execute() Result
+}
