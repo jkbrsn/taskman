a5ebbb60dbd5b0102d04431b5f7ffa20aaf48816
Jakob Ersson <11717405+jakobilobi@users.noreply.github.com>
2024-12-26 19:09:58 +0100

    test: add tests for priorityQueue

---
+++ priority_queue_test.go
@@ -0,0 +1,71 @@
+package taskman
+
+import (
+	"container/heap"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPushAndPop(t *testing.T) {
+	pq := &priorityQueue{}
+	heap.Init(pq)
+
+	job1 := &Job{ID: "job1", NextExec: time.Now().Add(10 * time.Second)} // Should be popped second
+	job2 := &Job{ID: "job2", NextExec: time.Now().Add(5 * time.Second)}  // Should be popped first
+	job3 := &Job{ID: "job3", NextExec: time.Now().Add(15 * time.Second)} // Should be popped last
+
+	heap.Push(pq, job1)
+	heap.Push(pq, job2)
+	heap.Push(pq, job3)
+
+	assert.Equal(t, "job2", heap.Pop(pq).(*Job).ID, "Expected job2 to be popped first")
+	assert.Equal(t, "job1", heap.Pop(pq).(*Job).ID, "Expected job1 to be popped second")
+	assert.Equal(t, "job3", heap.Pop(pq).(*Job).ID, "Expected job3 to be popped last")
+}
+
+func TestJobInQueue(t *testing.T) {
+	pq := &priorityQueue{}
+	heap.Init(pq)
+
+	job := &Job{ID: "job1", NextExec: time.Now().Add(10 * time.Second)}
+	heap.Push(pq, job)
+
+	index, err := pq.JobInQueue("job1")
+	assert.Nil(t, err, "Error when checking for job1 in queue")
+	assert.Equal(t, 0, index, "Expected job1 to be found at index 0")
+
+	_, err = pq.JobInQueue("job2")
+	assert.ErrorIs(t, err, ErrJobNotFound, "Expected job2 to not be found")
+}
+
+func TestPeek(t *testing.T) {
+	pq := &priorityQueue{}
+	heap.Init(pq)
+
+	job1 := &Job{ID: "job1", NextExec: time.Now().Add(10 * time.Second)}
+	job2 := &Job{ID: "job2", NextExec: time.Now().Add(5 * time.Second)}
+	heap.Push(pq, job1)
+	heap.Push(pq, job2)
+
+	assert.Equal(t, "job2", pq.Peek().ID, "Expected job2 to be peeked first")
+}
+
+func TestRemoveByID(t *testing.T) {
+	pq := &priorityQueue{}
+	heap.Init(pq)
+
+	job1 := &Job{ID: "job1", NextExec: time.Now().Add(10 * time.Second)}
+	job2 := &Job{ID: "job2", NextExec: time.Now().Add(5 * time.Second)}
+	heap.Push(pq, job1)
+	heap.Push(pq, job2)
+
+	err := pq.RemoveByID("job1")
+	assert.Nil(t, err, "Error when removing job1")
+
+	_, err = pq.JobInQueue("job1")
+	assert.ErrorIs(t, err, ErrJobNotFound, "Expected job1 to not be found after removal")
+
+	assert.Equal(t, 1, pq.Len(), "Expected queue length to be 1 after removal")
+}
